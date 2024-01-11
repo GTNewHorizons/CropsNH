111392ba5b1deb20fc32882cf87114eca444bf82
bombcar <github@bombcar.com>
2024-01-11 17:27:20 -0600

    move resourcefulcrops to cursemaven

--- src/api/java/tehnut/resourceful/crops/api/package-info.java
+++
@@ -1,8 +0,0 @@
-@API(owner = ID, apiVersion = VERSION, provides = APIID)
-package tehnut.resourceful.crops.api;
-
-import static tehnut.resourceful.crops.api.ModInformation.APIID;
-import static tehnut.resourceful.crops.api.ModInformation.ID;
-import static tehnut.resourceful.crops.api.ModInformation.VERSION;
-
-import cpw.mods.fml.common.API;
