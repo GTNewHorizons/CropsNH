4c36b46caae6e8bdac654db479a057c701f86d3f
bombcar <github@bombcar.com>
2024-01-12 12:41:29 -0600

    remove old apis and get them elsewhere

--- src/api/java/tehnut/resourceful/crops/api/package-info.java
+++
@@ -1,8 +0,0 @@
-@API(owner = ID, apiVersion = VERSION, provides = APIID)
-package tehnut.resourceful.crops.api;
-
-import cpw.mods.fml.common.API;
-
-import static tehnut.resourceful.crops.api.ModInformation.APIID;
-import static tehnut.resourceful.crops.api.ModInformation.ID;
-import static tehnut.resourceful.crops.api.ModInformation.VERSION;
