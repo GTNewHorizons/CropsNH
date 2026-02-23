e61685f7757c687f758f8735d08c44e098a83145
glowredman <35727266+glowredman@users.noreply.github.com>
2026-02-23 23:08:38 +0100

    Use Tags class

--- src/main/java/com/gtnewhorizon/cropsnh/reference/Reference.java
+++ src/main/java/com/gtnewhorizon/cropsnh/reference/Reference.java
@@ -1,14 +1,20 @@
 package com.gtnewhorizon.cropsnh.reference;
 
+import com.gtnewhorizon.cropsnh.Tags;
+
 public final class Reference {
     public static final String MOD_NAME = "CropsNH";
     public static final String MOD_ID = "CropsNH";
 
+    @Deprecated
     public static final String VER_MAJOR = "@MAJOR@";
+    @Deprecated
     public static final String VER_MINOR = "@MINOR@";
+    @Deprecated
     public static final String VER_REVIS = "@REVIS@";
+    @Deprecated
     public static final String MOD_VERSION = VER_MAJOR + "." + VER_MINOR + "." + VER_REVIS;
-    public static final String VERSION = "1.7.10-" + MOD_VERSION;
+    public static final String VERSION = Tags.VERSION;
 
     public static final String AUTHOR = "InfinityRaider";
     public static final String CLIENT_PROXY_CLASS = "com.gtnewhorizon.cropsnh.proxy.ClientProxy";
