7f96dc483c151a6661aa04e186afdbfb38c70dc5
Guillaume Mercier <C0bra5@users.noreply.github.com>
2026-03-03 10:02:58 -0500

    Update build deps, nerf soul sand lily and buff oil brerry (#9)

--- src/main/java/com/gtnewhorizon/cropsnh/crops/stoneilies/CropSoulSandLily.java
+++ src/main/java/com/gtnewhorizon/cropsnh/crops/stoneilies/CropSoulSandLily.java
@@ -15,7 +15,7 @@ public class CropSoulSandLily extends CropBaseStoneLily {
     public CropSoulSandLily() {
         super("soulSand", new Color(0x281A12), new Color(0x534034));
 
-        this.addDrop(new ItemStack(Blocks.soul_sand, 2), 100_00);
+        this.addDrop(new ItemStack(Blocks.soul_sand, 1), 100_00);
 
         this.addBlockUnderRequirement("soulSand");
 
