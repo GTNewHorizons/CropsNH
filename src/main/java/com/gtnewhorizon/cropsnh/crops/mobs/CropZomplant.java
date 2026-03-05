8e6f3afd57621f7292b80b0580c64b0430a5c9da
Guillaume Mercier <10gui-gui10@live.ca>
2026-03-05 14:18:54 -0500

    add zombie brain and head to zomplant
    The zombie head will be removed at a later date if/when I go though with the mod head crop.

--- src/main/java/com/gtnewhorizon/cropsnh/crops/mobs/CropZomplant.java
+++ src/main/java/com/gtnewhorizon/cropsnh/crops/mobs/CropZomplant.java
@@ -11,6 +11,7 @@ import com.gtnewhorizon.cropsnh.api.ISoilList;
 import com.gtnewhorizon.cropsnh.api.PlantRenderShape;
 import com.gtnewhorizon.cropsnh.crops.abstracts.NHCropCard;
 import com.gtnewhorizon.cropsnh.farming.registries.SoilRegistry;
+import com.gtnewhorizon.cropsnh.utility.ModUtils;
 
 public class CropZomplant extends NHCropCard {
 
@@ -19,7 +20,16 @@ public class CropZomplant extends NHCropCard {
     public CropZomplant() {
         super("zomplant", new Color(0x3A6529), new Color(0x679056));
 
-        this.addDrop(new ItemStack(Items.rotten_flesh, 1, 0), 100_00);
+        ItemStack rottenFlesh = new ItemStack(Items.rotten_flesh, 1, 0);
+        if (ModUtils.Thaumcraft.isModLoaded()) {
+            this.addDrop(rottenFlesh, 98_00);
+            // low chance but that's because with high gain you've basically got a 1/25 per harvest
+            this.addDrop(ModUtils.Thaumcraft.getStack("ItemZombieBrain", 1, 0), 1_50);
+        } else {
+            this.addDrop(rottenFlesh, 99_50);
+        }
+        // TODO: remove zombie head from drop table when the mob head crop is added.
+        this.addDrop(new ItemStack(Items.skull, 1, 2), 50);
 
         this.addLikedBiomes(BiomeDictionary.Type.DEAD, BiomeDictionary.Type.WASTELAND);
     }
