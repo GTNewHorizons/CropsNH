c26a607b51e6b1d448b5f626182ccf7182168c59
Guillaume Mercier <10gui-gui10@live.ca>
2026-03-22 21:34:36 -0400

    fix scanner not scanning crops in multiplayer

--- src/main/java/com/gtnewhorizon/cropsnh/proxy/CommonProxy.java
+++ src/main/java/com/gtnewhorizon/cropsnh/proxy/CommonProxy.java
@@ -1,13 +1,24 @@
 package com.gtnewhorizon.cropsnh.proxy;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.world.World;
+import static com.gtnewhorizon.gtnhlib.util.numberformatting.NumberFormatUtil.formatNumber;
 
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+
+import com.gtnewhorizon.cropsnh.api.IGrowthRequirement;
 import com.gtnewhorizon.cropsnh.compatibility.UiE.UtilitiesInExcessCompatHandler;
 import com.gtnewhorizon.cropsnh.handler.ConfigurationHandler;
+import com.gtnewhorizon.cropsnh.reference.Reference;
+import com.gtnewhorizon.cropsnh.tileentity.TileEntityCropSticks;
 import com.gtnewhorizon.cropsnh.utility.ModUtils;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import gregtech.api.events.BlockScanningEvent;
 
 @SuppressWarnings("unused")
 public abstract class CommonProxy implements IProxy {
@@ -25,6 +36,7 @@ public abstract class CommonProxy implements IProxy {
     @Override
     public void registerEventHandlers() {
         if (ModUtils.UtilitiesInExcess.isModLoaded()) UtilitiesInExcessCompatHandler.onRegeisteEventHandlers();
+        MinecraftForge.EVENT_BUS.register(new BlockScanningEventHandler());
     }
 
     @Override
@@ -32,4 +44,30 @@ public abstract class CommonProxy implements IProxy {
         ConfigurationHandler.init(event);
     }
 
+    public static class BlockScanningEventHandler {
+
+        @SubscribeEvent
+        public void onBlockScanned(BlockScanningEvent event) {
+            if (!(event.mTileEntity instanceof TileEntityCropSticks teCrop)) return;
+            event.mEUCost += 1000;
+            if (teCrop.hasCrop() && !teCrop.getSeed()
+                .getStats()
+                .isAnalyzed()) {
+                teCrop.getSeed()
+                    .setAnalyzed(true);
+            }
+            teCrop.getPlantLensStatus(event.mList);
+            event.mList.add(
+                StatCollector.translateToLocalFormatted(
+                    Reference.MOD_ID + "_tooltip.industrialFarm.scanner.6",
+                    formatNumber(teCrop.getNutrientScore()),
+                    formatNumber(TileEntityCropSticks.MAX_NUTRIENT_SCORE)));
+            List<IGrowthRequirement> failedReqs = teCrop.getFailedChecks();
+            if (failedReqs != null) {
+                for (IGrowthRequirement req : failedReqs) {
+                    event.mList.add(req.getDescription());
+                }
+            }
+        }
+    }
 }
