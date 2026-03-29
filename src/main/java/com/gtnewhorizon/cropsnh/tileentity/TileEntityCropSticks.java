38e1328f5ac7558b94008315feae6f3285121f44
Guillaume Mercier <C0bra5@users.noreply.github.com>
2026-03-29 13:44:41 -0400

    Fix drop stack sizes (#58)

--- src/main/java/com/gtnewhorizon/cropsnh/tileentity/TileEntityCropSticks.java
+++ src/main/java/com/gtnewhorizon/cropsnh/tileentity/TileEntityCropSticks.java
@@ -560,7 +560,7 @@ public class TileEntityCropSticks extends TileEntityCropsNH implements ICropStic
                 }
             }
             if (count > 0) {
-                int remaining = count * getSeedStack().stackSize + gainBonus;
+                int remaining = count * drop.getKey().stackSize + gainBonus;
                 while (remaining > 0) {
                     ItemStack stack = drop.getKey()
                         .copy();
