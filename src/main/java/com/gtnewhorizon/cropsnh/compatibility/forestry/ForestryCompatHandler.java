242edf98073a5a32c73733641a8801c3beb1d8b4
Guillaume Mercier <10gui-gui10@live.ca>
2026-04-01 08:39:28 -0400

    Allow CropsNH fertilizer in CropsNH

--- src/main/java/com/gtnewhorizon/cropsnh/compatibility/forestry/ForestryCompatHandler.java
+++ src/main/java/com/gtnewhorizon/cropsnh/compatibility/forestry/ForestryCompatHandler.java
@@ -2,13 +2,16 @@ package com.gtnewhorizon.cropsnh.compatibility.forestry;
 
 import java.util.ArrayList;
 
+import com.gtnewhorizon.cropsnh.api.CropsNHItemList;
 import com.gtnewhorizon.cropsnh.utility.ModUtils;
 
 import cpw.mods.fml.common.Optional;
 import forestry.api.farming.Farmables;
+import forestry.api.multiblock.MultiblockManager;
 import forestry.core.circuits.Circuit;
 import forestry.farming.circuits.CircuitFarmLogic;
 import forestry.farming.logic.FarmableReference;
+import forestry.plugins.PluginManager;
 
 public abstract class ForestryCompatHandler {
 
@@ -23,5 +26,9 @@ public abstract class ForestryCompatHandler {
         Farmables.farmables.putIfAbsent(FarmableReference.IC2Crops.get(), new ArrayList<>());
         Farmables.farmables.get(FarmableReference.IC2Crops.get())
             .add(new CropsNHForestryFarmable());
+
+        if (PluginManager.Module.FARMING.isEnabled()) {
+            MultiblockManager.farmFertilizerRegistry.addFertilizer(CropsNHItemList.fertilizer.get(1));
+        }
     }
 }
