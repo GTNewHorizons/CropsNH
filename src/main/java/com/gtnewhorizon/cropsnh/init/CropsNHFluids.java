aede8113c521e991c7169a34749524bef21dd48b
Guillaume Mercier <17766417+C0bra5@users.noreply.github.com>
2026-04-12 12:18:52 -0400

    Fix alcohol fluid langs and lessen gt++ reliance for celled fluids.

--- src/main/java/com/gtnewhorizon/cropsnh/init/CropsNHFluids.java
+++ src/main/java/com/gtnewhorizon/cropsnh/init/CropsNHFluids.java
@@ -1,74 +1,54 @@
 package com.gtnewhorizon.cropsnh.init;
 
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidStack;
+import java.awt.Color;
 
 import com.gtnewhorizon.cropsnh.api.CropsNHItemList;
 import com.gtnewhorizon.cropsnh.fluids.AlcoholImpure;
+import com.gtnewhorizon.cropsnh.fluids.CropsNHCellFluid;
 import com.gtnewhorizon.cropsnh.reference.Reference;
-
-import cpw.mods.fml.common.LoaderException;
-import gregtech.api.enums.ItemList;
-import gtPlusPlus.core.util.minecraft.FluidUtils;
 
 public class CropsNHFluids {
 
-    public static Fluid fertilizer;
-    public static Fluid enrichedFertilizer;
+    // fertilizers
+    public static CropsNHCellFluid fertilizer;
+    public static CropsNHCellFluid enrichedFertilizer;
 
     // alchool
-    public final static AlcoholImpure FWheat = new AlcoholImpure(Reference.MOD_ID + ":FWheat", 40, "potion.alcopops");
-    public final static AlcoholImpure Korn = new AlcoholImpure(Reference.MOD_ID + ":Korn", 320, "potion.vodka");
-    public final static AlcoholImpure DKorn = new AlcoholImpure(Reference.MOD_ID + ":DKorn", 380, "potion.vodka");
-    public final static AlcoholImpure FReed = new AlcoholImpure(Reference.MOD_ID + ":FReed", 50, "potion.alcopops");
-    public final static AlcoholImpure SWhine = new AlcoholImpure(Reference.MOD_ID + ":SWhine", 700, "potion.reedwater");
-    public final static AlcoholImpure Mash = new AlcoholImpure(Reference.MOD_ID + ":Mash", 25, "potion.reedwater");
-    public final static AlcoholImpure Wash = new AlcoholImpure(Reference.MOD_ID + ":Wash", 50, "potion.alcopops");
-    public final static AlcoholImpure GHP = new AlcoholImpure(Reference.MOD_ID + ":GHP", 700, "potion.vodka");
-    public final static AlcoholImpure jagi = new AlcoholImpure(Reference.MOD_ID + ":jagi", 100000, "potion.alcopops");
-    public final static AlcoholImpure njagi = new AlcoholImpure(Reference.MOD_ID + ":njagi", 350, "potion.alcopops");
+    public static AlcoholImpure FWheat;
+    public static AlcoholImpure Korn;
+    public static AlcoholImpure DKorn;
+    public static AlcoholImpure FReed;
+    public static AlcoholImpure SWhine;
+    public static AlcoholImpure Mash;
+    public static AlcoholImpure Wash;
+    public static AlcoholImpure GHP;
+    public static AlcoholImpure jagi;
+    public static AlcoholImpure njagi;
 
     public static void preInit() {
-        // could probably use a propper GT Material but this is faster
-        CropsNHFluids.enrichedFertilizer = FluidUtils.addGTFluidNonMolten(
-            Reference.MOD_ID + ".enrichedFertilizer",
-            "Enriched Fertilizer",
-            new short[] { 40, 229, 21, 100 },
-            4,
-            295,
-            null,
-            ItemList.Cell_Empty.get(1),
-            1000,
-            true);
 
-        CropsNHFluids.fertilizer = FluidUtils.addGTFluidNonMolten(
+        // using . instead of : because backwards compatibility
+        fertilizer = new CropsNHCellFluid(
             Reference.MOD_ID + ".fertilizer",
-            "Fertilizer",
-            new short[] { 45, 170, 45, 100 },
-            4,
-            32,
-            null,
-            ItemList.Cell_Empty.get(1),
-            1000,
-            true);
+            new Color(45, 170, 45, 100),
+            "fertilizerCell",
+            CropsNHItemList.fertilizerCell);
 
-        CropsNHItemList.enrichedFertilizerCell.set(
-            FluidContainerRegistry.fillFluidContainer(
-                new FluidStack(CropsNHFluids.enrichedFertilizer, 1000),
-                ItemList.Cell_Empty.get(1)));
-        CropsNHItemList.fertilizerCell.set(
-            FluidContainerRegistry
-                .fillFluidContainer(new FluidStack(CropsNHFluids.fertilizer, 1000), ItemList.Cell_Empty.get(1)));
-        try {
-            CropsNHItemList.fertilizer.get(1);
-        } catch (NullPointerException npe) {
-            throw new LoaderException(npe);
-        }
-        try {
-            CropsNHItemList.enrichedFertilizerCell.get(1);
-        } catch (NullPointerException npe) {
-            throw new LoaderException(npe);
-        }
+        enrichedFertilizer = new CropsNHCellFluid(
+            Reference.MOD_ID + ".enrichedFertilizer",
+            new Color(40, 229, 21, 100),
+            "enrichedFertilizerCell",
+            CropsNHItemList.enrichedFertilizerCell);
+
+        FWheat = new AlcoholImpure(Reference.MOD_ID + ":FWheat", 40, "potion.alcopops");
+        Korn = new AlcoholImpure(Reference.MOD_ID + ":Korn", 320, "potion.vodka");
+        DKorn = new AlcoholImpure(Reference.MOD_ID + ":DKorn", 380, "potion.vodka");
+        FReed = new AlcoholImpure(Reference.MOD_ID + ":FReed", 50, "potion.alcopops");
+        SWhine = new AlcoholImpure(Reference.MOD_ID + ":SWhine", 700, "potion.reedwater");
+        Mash = new AlcoholImpure(Reference.MOD_ID + ":Mash", 25, "potion.reedwater");
+        Wash = new AlcoholImpure(Reference.MOD_ID + ":Wash", 50, "potion.alcopops");
+        GHP = new AlcoholImpure(Reference.MOD_ID + ":GHP", 700, "potion.vodka");
+        jagi = new AlcoholImpure(Reference.MOD_ID + ":jagi", 100000, "potion.alcopops");
+        njagi = new AlcoholImpure(Reference.MOD_ID + ":njagi", 350, "potion.alcopops");
     }
 }
