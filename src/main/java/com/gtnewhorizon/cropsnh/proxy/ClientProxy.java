137fd90af3da937d3d6755c68548f6c4d3fcad01
Guillaume Mercier <C0bra5@users.noreply.github.com>
2026-07-11 08:11:13 -0400

    Remove unused proxy functions (#208)
    Co-authored-by: Guillaume Mercier <17766417+C0bra5@users.noreply.github.com>

--- src/main/java/com/gtnewhorizon/cropsnh/proxy/ClientProxy.java
+++ src/main/java/com/gtnewhorizon/cropsnh/proxy/ClientProxy.java
@@ -1,14 +1,8 @@
 package com.gtnewhorizon.cropsnh.proxy;
 
 import java.lang.reflect.Field;
-import java.util.Iterator;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 
 import com.gtnewhorizon.cropsnh.blocks.abstracts.BlockCropsNH;
 import com.gtnewhorizon.cropsnh.handler.ConfigurationHandler;
@@ -16,36 +10,10 @@ import com.gtnewhorizon.cropsnh.init.CropsNHBlocks;
 import com.gtnewhorizon.cropsnh.renderers.blocks.RenderBlockBase;
 import com.gtnewhorizon.cropsnh.utility.LogHelper;
 
-import codechicken.nei.api.API;
-import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @SuppressWarnings("unused")
 public class ClientProxy extends CommonProxy {
-
-    @Override
-    public EntityPlayer getClientPlayer() {
-        return Minecraft.getMinecraft().thePlayer;
-    }
-
-    @Override
-    public World getClientWorld() {
-        return Minecraft.getMinecraft().theWorld;
-    }
-
-    @Override
-    public World getWorldByDimensionId(int dimension) {
-        return FMLClientHandler.instance()
-            .getServer()
-            .worldServerForDimension(dimension);
-    }
-
-    @Override
-    public Entity getEntityById(World world, int id) {
-        return world.getEntityByID(id);
-    }
 
     @Override
     public void registerRenderers() {
@@ -71,36 +39,6 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void registerEventHandlers() {
         super.registerEventHandlers();
-    }
-
-    @Override
-    public void hideItemInNEI(ItemStack stack) {
-        Iterator<ModContainer> mods = Loader.instance()
-            .getActiveModList()
-            .iterator();
-        ModContainer modContainer;
-        while (mods.hasNext()) {
-            modContainer = (ModContainer) mods.next();
-            if (modContainer.getModId()
-                .equalsIgnoreCase("NotEnoughItems")) {
-                API.hideItem(stack);
-            }
-        }
-    }
-
-    @Override
-    public void addItemInNEI(ItemStack stack) {
-        Iterator<ModContainer> mods = Loader.instance()
-            .getActiveModList()
-            .iterator();
-        ModContainer modContainer;
-        while (mods.hasNext()) {
-            modContainer = (ModContainer) mods.next();
-            if (modContainer.getModId()
-                .equalsIgnoreCase("NotEnoughItems")) {
-                API.addItemVariant(stack.getItem(), stack);
-            }
-        }
     }
 
     @Override
